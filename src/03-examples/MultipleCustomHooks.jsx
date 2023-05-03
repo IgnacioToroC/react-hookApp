689263e8dbbc64de916b16d7e49b22fd06638ed2
Ignacio <Ignacio.toro.carvajal@gmail.com>
2023-05-02 22:06:33 -0400

    Union Hooks

--- src/03-examples/MultipleCustomHooks.jsx
+++ src/03-examples/MultipleCustomHooks.jsx
@@ -1,10 +1,27 @@
-import { useFetch } from "../hooks/useFetch";
+import { useFetch, useCounter } from "../hooks/index";
+
+import { LoadingQuote, Quote } from "./index";
 
 export const MultipleCustomHooks = () => {
 
-    const { data, isLoading, hasError } = useFetch('https://api.breakingbadquotes.xyz/v1/quotes/5');
+    const { counter, increment } = useCounter(1);
+    
+    const { data, isLoading, hasError } = useFetch(`https://api.breakingbadquotes.xyz/v1/quotes/${ counter }`);
 
-console.log(data, isLoading, hasError)
+    const  { author, quote } = !!data && data[0];
+    //console.log(data)
+
+    // if( isLoading ){
+    //     return(
+    //         <>
+    //             <br />
+    //             <br />
+    //             <br />
+    //             <h1>Cargando...</h1> 
+    //             <hr /> 
+    //         </>
+    //     )
+    // }
 
     return (
         <>
@@ -12,7 +29,16 @@ console.log(data, isLoading, hasError)
             <br />
             <br />
             <h1>Braking bad Quotes</h1>
-
+            
+           { 
+                ( isLoading ) 
+                ? <LoadingQuote />
+                : <Quote author={ author } quote={ quote } />   
+            }
+           
+           <button className="btn btn-primary" disabled= { isLoading } onClick={
+            () => increment(1)
+           }  >Next quote</button>
 
             <hr />
         </>
