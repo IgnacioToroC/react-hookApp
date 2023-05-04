3559f6f4cf80236b214354bbde7235ff1824b4a0
Ignacio <Ignacio.toro.carvajal@gmail.com>
2023-05-03 20:26:21 -0400

    useRef

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook';
 import { SimpleForm } from './02-useEffect/SimpleForm';
 import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
-
+import { FocusScreen } from './04-useRef/FocusScreen';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -17,5 +17,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <SimpleForm />
     <FormWithCustomHook />
     <MultipleCustomHooks /> 
+    <FocusScreen></FocusScreen>
+
   </React.StrictMode>,
 )
