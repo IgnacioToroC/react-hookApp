933093fc2abf356a48a9538dda3d9285981bf3cc
Ignacio <Ignacio.toro.carvajal@gmail.com>
2023-05-06 21:58:15 -0400

    useMemo

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 import { FocusScreen } from './04-useRef/FocusScreen';
 import { Layout } from './05-useLayoutEffect/Layout';
 import { Memorize } from './06-memos/Memorize';
-
+import { MemoHook } from './06-memos/MemoHook';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
@@ -24,5 +24,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <FocusScreen></FocusScreen>
     <Layout></Layout>
     <Memorize></Memorize>
+    <MemoHook/>
   </React.StrictMode>,
 )
