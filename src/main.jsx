d247689c0171aeba2344ba9a135337e9f453a982
Ignacio <Ignacio.toro.carvajal@gmail.com>
2023-05-06 21:37:23 -0400

    memorize

--- src/main.jsx
+++ src/main.jsx
@@ -9,6 +9,9 @@ import { FormWithCustomHook } from './02-useEffect/FormWithCustomHook';
 import { MultipleCustomHooks } from './03-examples/MultipleCustomHooks';
 import { FocusScreen } from './04-useRef/FocusScreen';
 import { Layout } from './05-useLayoutEffect/Layout';
+import { Memorize } from './06-memos/Memorize';
+
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -20,5 +23,6 @@ ReactDOM.createRoot(document.getElementById('root')).render(
     <MultipleCustomHooks /> 
     <FocusScreen></FocusScreen>
     <Layout></Layout>
+    <Memorize></Memorize>
   </React.StrictMode>,
 )
