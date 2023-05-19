7062664f7c83d18deb77c99096a1992e2c080aef
Ignacio <Ignacio.toro.carvajal@gmail.com>
2023-05-19 19:00:38 -0400

    todoReducer

--- src/08-useReducer/into-reducer.js
+++ src/08-useReducer/into-reducer.js
@@ -1,1 +1,32 @@
-console.log("hola mundo")
+const initialState =[{
+    id:1,
+    todo:'Recolectar la piedra del alma',
+    done:false
+}];
+
+
+const todoReducer = ( state = initialState, action ={}) =>{
+
+    if( action.type === '[TODO] ass todo'){
+        return [ ...state, action.payload]
+    }
+
+    return state;
+}
+
+
+let todos = todoReducer()
+
+
+const newTodo ={
+    id:2,
+    todo:'Recolectar la piedra del poder',
+    done:false  
+}
+
+const addTodoAction ={
+    type:'[TODO] ass todo',
+    payload: newTodo,
+}
+ todos = todoReducer( todos, addTodoAction)
+console.log(todos)
